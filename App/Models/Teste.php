5b0ded334bd14408df45bebf68d1f4ebd89a567b
Aparecido de Oliveira Muniz <cidoliveiraa@gmail.com>
2018-08-30 17:10:42 -0300

    #2

--- App/Models/Teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace model;
-
-class Teste{
-    public function Greetz($nome){
-        echo "<div class='jumbotron pt-0 pb-0 h1'>Oi, eu sou o {$nome}</div>"; 
-    }
-}
--- App/Models/View.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Model;
-
-class View{
-    public function getMenuPrincipal(){
-        require("App/Views/Includes/Menu.php");
-    }
-}
